11d50110c109e239f03e4f735c32e89fbac40e28
mflanders06 <mflanders06@gmail.com>
2021-06-24 20:51:49 -0600

    Last steps

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const massive = require('massive');
 const session = require('express-session');
 const authCtrl = require('./controllers/authController');
 const treasureCtrl = require('./controllers/treasureController')
+const auth = require('./middleware/authMiddleware');
 
 const app = express();
 
@@ -35,3 +36,7 @@ massive({
 app.post('/auth/register', authCtrl.register);
 app.post('/auth/login', authCtrl.login);
 app.get('/auth/logout', authCtrl.logout);
+
+app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure);
+app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure);
+app.post('/api/treasure/user', auth.usersOnly, treasureCtrl.addUserTreasure)
